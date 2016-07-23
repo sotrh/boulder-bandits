1e224ede81ef9c41d01f823d40e573db11209668
sotrh <bhbenjaminhansen@gmail.com>
2016-07-23 17:03:05 -0600

    inlined some extensions

--- core/src/io/sotrh/boulderbandits/util/Extensions.kt
+++ core/src/io/sotrh/boulderbandits/util/Extensions.kt
@@ -17,12 +17,12 @@ fun Float.p2m(scale:Float = PIXELS_TO_METERS) = this / scale
 fun Float.m2p(scale: Float = PIXELS_TO_METERS) = this * scale
 
 // shape renderer extensions
-fun ShapeRenderer.doRender(shapeType:ShapeRenderer.ShapeType, block:ShapeRenderer.()->Unit) {
+inline fun ShapeRenderer.doRender(shapeType:ShapeRenderer.ShapeType, block:ShapeRenderer.()->Unit) {
     begin(shapeType)
     block()
     end()
 }
-fun ShapeRenderer.doRender(camera: Camera, shapeType: ShapeRenderer.ShapeType = ShapeRenderer.ShapeType.Filled, block: ShapeRenderer.() -> Unit) {
+inline fun ShapeRenderer.doRender(camera: Camera, shapeType: ShapeRenderer.ShapeType = ShapeRenderer.ShapeType.Filled, block: ShapeRenderer.() -> Unit) {
     projectionMatrix = camera.combined
     doRender(shapeType, block)
 }
